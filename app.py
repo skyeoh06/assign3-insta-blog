59199b4c9edef3541229eab5f097c6fc5062f03d
skyeoh06 <felyciayeoh@yahoo.com>
2020-08-02 04:34:06 +0000

    setting debug to false

--- app.py
+++ app.py
@@ -330,4 +330,4 @@ def process_delete_blog(id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
