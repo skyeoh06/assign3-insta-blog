9194f5121217e4940a4bc890fecd89989d7f1bb3
skyeoh06 <felyciayeoh@yahoo.com>
2020-08-29 14:35:03 +0000

    edit navbar open and closed margin

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("home").style.marginLeft = "200px";
+    document.getElementById("home").style.marginLeft = "0";
     
     
 }
   
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("home").style.marginLeft= "0";
+    document.getElementById("home").style.marginLeft= "100px";
 }
 
